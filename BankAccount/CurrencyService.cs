91be715edf60c9316c84ec19206610e56f297477
Joshua Partogi <proscrumtrainer@live.com>
2016-05-30 00:27:26 +0800

    Add Currency service and mocker

---
+++ BankAccount/CurrencyService.cs
@@ -0,0 +1,20 @@
+﻿namespace BankAccount
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class CurrencyService : ICurrencyService
+    {
+        private IDictionary<string, double> CurrencyRates = new Dictionary<string, double>();
+
+        public void AddCurrencyRate(string fromCurrency, string toCurrency, double rate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDictionary<string, double> GetCurrencyRates()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BankAccount/ICurrencyService.cs
@@ -0,0 +1,11 @@
+﻿namespace BankAccount
+{
+    using System.Collections.Generic;
+
+    public interface ICurrencyService
+    {
+        void AddCurrencyRate(string fromCurrency, string toCurrency, double rate);
+
+        IDictionary<string, double> GetCurrencyRates();
+    }
+}
